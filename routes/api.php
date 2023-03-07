82600d5093f6694b575ec674cc3c827b29be015a
Anthenny <anthenny1997@hotmail.com>
2023-03-07 14:32:08 +0100

    Sanctum toegevoegd en routes georganiseerd

--- routes/api.php
+++ routes/api.php
@@ -2,32 +2,21 @@
 
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\ProductController;
-use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Public routes
+Route::get("/products", [ProductController::class, "index"]);
+Route::get("/products/{product}", [ProductController::class, "show"]);
+Route::get("/products/search/{name}", [ProductController::class, "search"]);
 
-Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
-    return $request->user();
+// Protected routes
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::post("/products", [ProductController::class, "store"]);
+    Route::put("/products/{product}", [ProductController::class, "update"]);
+    Route::delete("/products/{product}", [ProductController::class, "destroy"]);
 });
 
-Route::get("/listings", [ListingController::class, "index"]);
-Route::get("/listing", [ListingController::class, "show"]);
-Route::post("/listing", [ListingController::class, "create"]);
-
-Route::get("/products", [ProductController::class, "index"]);
-Route::put("/products/{product}", [ProductController::class, "update"]);
-Route::get("/products/{product}", [ProductController::class, "show"]);
-Route::delete("/products/{product}", [ProductController::class, "destroy"]);
-Route::post("/products", [ProductController::class, "store"]);
-Route::get("/products/search/{name}", [ProductController::class, "search"]);
+// Overig
+// Route::get("/listings", [ListingController::class, "index"]);
+// Route::get("/listing", [ListingController::class, "show"]);
+// Route::post("/listing", [ListingController::class, "create"]);
